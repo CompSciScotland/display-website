7e24ce040757ffeac4354ce436da236cff627ab5
Hariss Ali Gills <harissgills@gmail.com>
2022-09-11 13:37:36 +0100

    Button functioality

--- src/lib/ButtonGroup.svelte
+++ src/lib/ButtonGroup.svelte
@@ -1,7 +1,27 @@
+<script>
+  let current = "early";
+</script>
+
 <div class="btn-group mb-3">
-    <button class="btn btn-active">Early Levels</button>
-    <button class="btn">First Level</button>
-    <button class="btn">Second Level</button>
-    <button class="btn">Third/Fourth Level</button>
-    <button class="btn">Senior Level</button>
-  </div>
+  <button
+    class:btn-active={current === "early"} class="btn"
+    on:click={() => (current = "early")}>Early Levels</button
+  >
+  <button
+  class:btn-active={current === "first"} class="btn"
+  on:click={() => (current = "first")}
+    >First Level</button
+  >
+  <button
+  class:btn-active={current === "second"} class="btn"
+  on:click={() => (current = "second")}>Second Level</button
+  >
+  <button
+    class:btn-active={current === "t/f"} class="btn"
+    on:click={() => (current = "t/f")}>Third/Fourth Level</button
+  >
+  <button
+    class:btn-active={current === "senior"} class="btn"
+    on:click={() => (current = "senior")}>Senior Level</button
+  >
+</div>
