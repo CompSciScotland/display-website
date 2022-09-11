d8b0419d592c4465f7a583b9e6b10889f84ab5e5
Hariss Ali Gills <harissgills@gmail.com>
2022-09-11 12:21:20 +0100

    Added heading and button

---
+++ src/lib/ButtonGroup.svelte
@@ -0,0 +1,5 @@
+<div class="btn-group">
+    <button class="btn btn-active">Button</button>
+    <button class="btn">Button</button>
+    <button class="btn">Button</button>
+  </div>
---
+++ src/lib/Drive.svelte
@@ -0,0 +1,6 @@
+<script>
+    import ButtonGroup from "./ButtonGroup.svelte";
+</script>
+
+<ButtonGroup/>
+<iframe title="Drive iframe" src="https://drive.google.com/embeddedfolderview?id=1WvcTAlYoGEo4kWSkj_qKYXVps_qn7PQt#list" width="100%" height="500" frameborder="0"></iframe>
